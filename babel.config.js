1d78d861077f9ab0f3807f644f72a4493eef80e4
ho-stanley <70745911+ho-stanley@users.noreply.github.com>
2023-05-09 20:07:57 +0200

    Initial layout setup (#1)
    * chore: add react-native-paper component library
    
    * chore: update react-native-paper project config
    
    * chore: add react-native-paper provider in root component
    
    * chore: add react navigation dependency
    
    * chore: move wrapper components to App.tsx
    
    * chore: add babel module resolver
    
    * feat: add draft layout for Home
    
    * feat: add button to add feed
    
    * feat: add tooltip to add feed button
    
    * feat: add icon on left side to feed title

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,30 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  env: {
+    production: {
+      plugins: ['react-native-paper/babel'],
+    },
+  },
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+  ],
 };
