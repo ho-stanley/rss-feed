6f2763015c560d4afc6f054ee1ab97ac1c778be7
Stanley Ho <70745911+ho-stanley@users.noreply.github.com>
2023-05-18 16:38:54 +0200

    refactor: change to Stack navigator instead of Native stack

--- App.tsx
+++ App.tsx
@@ -1,17 +1,18 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createStackNavigator} from '@react-navigation/stack';
 import Feed from '@/navigation/screens/Feed';
 import Edit from '@/navigation/screens/Edit';
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 function App(): JSX.Element {
   return (
     <PaperProvider>
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator screenOptions={{headerTitleAlign: 'center'}}>
           <Stack.Screen
             name="Feed"
             component={Feed}
