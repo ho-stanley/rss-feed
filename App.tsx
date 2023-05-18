ccc5be72f3bccb18053e6fa89cc6bf5fe712f963
Stanley Ho <70745911+ho-stanley@users.noreply.github.com>
2023-05-18 14:38:51 +0200

    feat: add stack navigator to transition between screens

--- App.tsx
+++ App.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Feed from '@/navigation/screens/Feed';
+import Edit from '@/navigation/screens/Edit';
+
+const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
   return (
     <PaperProvider>
       <NavigationContainer>
-        <Feed />
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Feed"
+            component={Feed}
+            options={{title: 'Feed'}}
+          />
+          <Stack.Screen
+            name="Edit"
+            component={Edit}
+            options={{title: 'Edit'}}
+          />
+        </Stack.Navigator>
       </NavigationContainer>
     </PaperProvider>
   );
