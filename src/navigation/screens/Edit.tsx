b12d7e0d154609b24c0eddf0190c87fa7ec122e9
Stanley Ho <70745911+ho-stanley@users.noreply.github.com>
2023-05-18 14:37:43 +0200

    feat: add initial Edit screen for feed

---
+++ src/navigation/screens/Edit.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text} from 'react-native-paper';
+import {ScrollView} from 'react-native';
+
+function Edit(): JSX.Element {
+  return (
+    <ScrollView>
+      <Text>Edit feed</Text>
+    </ScrollView>
+  );
+}
+
+export default Edit;
