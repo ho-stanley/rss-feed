c104a86922d44ad07331d045d0ab54d8c3b8ac25
Stanley Ho <70745911+ho-stanley@users.noreply.github.com>
2023-06-11 22:56:13 +0200

    feat: add header for action screens

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import 'react-native-gesture-handler';
+import 'react-native-gesture-handler'; // Must be imported first!
 import React from 'react';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {NavigationContainer} from '@react-navigation/native';
@@ -8,9 +8,11 @@ import FeedsScreen from '@/navigation/screens/Feeds';
 import EditFeedScreen from '@/navigation/screens/EditFeed';
 import {RootStackParamList} from '@/navigation/types';
 import AppbarHeader from '@/navigation/AppbarHeader';
+import ActionHeader from '@/navigation/ActionHeader';
 
 const RootStack = createStackNavigator<RootStackParamList>();
 const AppHeader = (props: StackHeaderProps) => <AppbarHeader {...props} />;
+const ModalHeader = (props: StackHeaderProps) => <ActionHeader {...props} />;
 
 function App(): JSX.Element {
   return (
@@ -29,11 +31,15 @@ function App(): JSX.Element {
               options={{title: 'Edit feed'}}
             />
           </RootStack.Group>
-          <RootStack.Group screenOptions={{presentation: 'modal'}}>
+          <RootStack.Group
+            screenOptions={{
+              presentation: 'modal',
+              header: ModalHeader,
+            }}>
             <RootStack.Screen
               name="AddFeed"
               component={AddFeedScreen}
-              options={{title: 'Add feed', headerShown: false}}
+              options={{title: 'Add feed'}}
             />
           </RootStack.Group>
         </RootStack.Navigator>
